7495e476479e4f05ba8bd93daf58ac43bfda9d39
Janiele Vilar <vilarjaniele@gmail.com>
2022-03-23 08:00:51 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "NotFound"', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Testa se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/Pikachu crying because the page requested.../i);
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
